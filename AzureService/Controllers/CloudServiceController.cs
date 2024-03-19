91a7bddeb8aa0b9a1214525c4328f9421a8731a2
Dermot Boyle <dermotb@hotmail.com>
2024-03-19 21:55:35 +0000

    Add project files.

---
+++ AzureService/Controllers/CloudServiceController.cs
@@ -0,0 +1,39 @@
+﻿using AzureService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AzureService.Controllers
+{
+    public class CloudServiceController : Controller
+    {
+        // GET: CloudServiceController
+        public ActionResult Calculate()
+        {
+            ViewBag.InstanceSize = new SelectList(CloudService.InstanceSizeDescriptions);
+            return View(new CloudService() { NoInstances=2});
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(CloudService svc)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Confirm", svc);
+            }
+            else
+            {
+                ViewBag.InstanceSize = new SelectList(CloudService.InstanceSizeDescriptions);
+                return View(svc);
+            }
+        }
+
+        // GET: CloudServiceController/Create
+        public ActionResult Confirm(CloudService service)
+        {
+            return View(service);
+        }
+
+
+    }
+}
---
+++ AzureService/Models/CloudService.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+
+namespace AzureService.Models
+{
+    public class CloudService
+    {
+        public static String[] InstanceSizeDescriptions
+        {
+            get
+            {
+                return new String[] { "Very Small", "Small", "Medium", "Large", "Very Large", "A5", "A6" };
+            }
+        }
+
+        public static double[] InstanceSizePrices
+        {
+            get
+            {
+                return new double[] { 0.02, 0.08, 0.16, 0.32, 0.64, 0.90, 1.80 };
+            }
+        }
+
+        [DisplayName("No of Instances")]
+        public int NoInstances { get; set; }
+
+        [DisplayName("Instance Size")]
+        public string  InstanceSize { get; set; }
+
+        public double Cost
+        {
+            get 
+            {
+                int size = 0;
+                for (int i=0; i<  CloudService.InstanceSizeDescriptions.Length; i++)
+                {
+                    if (CloudService.InstanceSizeDescriptions[i] == this.InstanceSize)
+                    {
+                        size = i;
+                        break;
+                    }
+                }
+
+                double hourlyPrice = NoInstances * InstanceSizePrices[size];
+                double dailyPrice = hourlyPrice * 24;
+                double yearlyPrice;
+
+                if (DateTime.IsLeapYear(DateTime.Now.Year))
+                {
+                    yearlyPrice = dailyPrice * 366;
+                }
+                else
+                {
+                    yearlyPrice = dailyPrice * 365;
+                }
+
+                return yearlyPrice;
+            }
+        }
+    }
+}
